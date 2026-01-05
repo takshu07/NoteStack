a90e2372760c78f4a6cdba4dfabf8103f406012d
takshu07 <takshubudhlakoti123@gmail.com>
2026-01-05 15:33:37 +0530

    Initial MERN TypeScript setup

---
+++ backend/src/config/db.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const connectDB = async (): Promise<void> => {
+  try {
+    const mongoDbUrl = process.env.MONGODB_URL;
+
+    if (!mongoDbUrl) {
+      throw new Error("MONGO_URI not defined in environment variables");
+    }
+
+    await mongoose.connect(mongoDbUrl);
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
